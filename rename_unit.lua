c54b069525a5728933e9bb6e70e0aed1660febca
peterpji <peterpji@hotmail.com>
2021-05-18 12:51:16 +0300

    Remove irrelevant code

--- rename_unit.lua
+++ rename_unit.lua
@@ -7,26 +7,6 @@ local RenameCounts = {
     TECH3 = 0
 }
 
-
-function GetNameCommander(username, unit)
-    local Ukills = unit:GetStat('KILLS', 0).Value
-    local newName
-    if Ukills >= unit:GetBlueprint().Veteran.Level5 then
-        newName = "<[["..username.."]]>"
-    elseif Ukills >= unit:GetBlueprint().Veteran.Level4 then
-        newName = "[["..username.."]]"
-    elseif Ukills >= unit:GetBlueprint().Veteran.Level3 then
-        newName = "<["..username.."]>"
-    elseif Ukills >= unit:GetBlueprint().Veteran.Level2 then
-        newName = "["..username.."]"
-    elseif Ukills >= unit:GetBlueprint().Veteran.Level1 then
-        newName = "<"..username..">"
-    else
-        newName = username
-    end
-    return newName
-end
-
 function GetTier(unit)
     local found_tier
     for _, tier in techCategories do
@@ -57,47 +37,16 @@ end
 function RenameUnit(username, unit)
     local tier = GetTier(unit)
     if unit['IsChecked'] == nil and string.find(unit:GetBlueprint().Description, 'Interceptor') == nil and unit:IsInCategory('STRUCTURE') == false and RandomNamingRate(RenameCounts[tier]) and ( unit:GetCustomName(unit) == nil or unit:IsInCategory('COMMAND') == true ) then
-        local newName
-        local temptable ;
-        if unit:IsInCategory('COMMAND') == true then
-            newName = GetNameCommander(username, unit)
+        local chosenNameTable
+        if unit:IsInCategory('EXPERIMENTAL') then
+            chosenNameTable = NameTable.Experimental
         else
-            if unit:IsInCategory('EXPERIMENTAL') then
-                if unit:IsInCategory('UEF') then
-                    temptable = NameTable.UEFT4table
-                elseif unit:IsInCategory('CYBRAN') then
-                    temptable = NameTable.CybranT4table
-                elseif unit:IsInCategory('AEON') then
-                    temptable = NameTable.AeonT4table
-                elseif unit:IsInCategory('SERAPHIM') then
-                    temptable = NameTable.SeraphimT4table
-                end
-            elseif unit:IsInCategory('DEFENSE') then
-                temptable = NameTable.Defense
-            elseif unit:IsInCategory('STRUCTURE') then
-                temptable = NameTable.Structures
-            elseif unit:IsInCategory('BATTLESHIP') == true or unit:IsInCategory('BATTLECRUISER') then
-                temptable = NameTable.BNaval
-            elseif unit:IsInCategory('NAVAL') then
-                temptable = NameTable.Naval
-            elseif unit:IsInCategory('AIR') and unit:IsInCategory('GROUNDATTACK') then
-                temptable = NameTable.Gunships
-            elseif unit:IsInCategory('AIR') then
-                temptable = NameTable.AirTable
-            else
-                temptable = NameTable.Default
-            end
-            if temptable ~= nil then
-                newName = temptable[math.random(table.getsize (temptable) )]
-            end
+            chosenNameTable = NameTable.Default
         end
-        if newName ~= nil then
-            unit:SetCustomName(newName)
-            if tier ~= nil then
-                RenameCounts[tier] = 1 + RenameCounts[tier]
-            end
-        else
-            unit:SetCustomName("test")
+        local newName = chosenNameTable[math.random(table.getsize(chosenNameTable))]
+        unit:SetCustomName(newName)
+        if tier ~= nil then
+            RenameCounts[tier] = 1 + RenameCounts[tier]
         end
     end
     unit['IsChecked'] = true
