81e4bd32918670f76d81a6a02f61997d09625467
peterpji <peterpji@hotmail.com>
2021-05-18 14:51:34 +0300

    Add author to name lists

--- parse_tables_from_patreons.py
+++ parse_tables_from_patreons.py
@@ -4,19 +4,27 @@ import pandas as pd
 T4_THRESHOLD = 10
 SOURCE_FILE = 'patronnamemod.csv'
 
+EXTRA_NAMES = ['peterpji']
 DIR = os.path.dirname(__file__)
 SCRIPT_HEAD = "local NameTable = {\nExperimental = {\n"
 SCRIPT_TAIL = "}}\nfunction GetTable()\nreturn NameTable;\nend"
 
 
+def add_extra_names(out):
+    for name in EXTRA_NAMES:
+        out.write(f'"{name}",\n')
+
+
 def write_t4_names(data, out):
     for name in data[data['Pledge Amount'] > T4_THRESHOLD].Name:
         out.write(f'"{name}",\n')
+    add_extra_names(out)
 
 
 def write_default_names(data, out):
     for name in data.Name:
         out.write(f'"{name}",\n')
+    add_extra_names(out)
 
 
 def main():
