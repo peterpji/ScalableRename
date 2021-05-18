fe3c449edfcfcb9f66f045670e20db0699328e3c
peterpji <peterpji@hotmail.com>
2021-05-18 12:05:47 +0300

    Count how many units per tier has been created

--- rename_unit.lua
+++ rename_unit.lua
@@ -1,4 +1,12 @@
 local NameTable = import("/mods/ScalableRename/tables.lua").GetTable()
+local domainCategories = { "NAVAL", "LAND", "AIR" }
+local techCategories = { "TECH1", "TECH2", "TECH3" }
+local RenameCounts = {
+    TECH1 = 0,
+    TECH2 = 0,
+    TECH3 = 0
+}
+
 
 function GetNameCommander(username, unit)
     local Ukills = unit:GetStat('KILLS', 0).Value
@@ -19,8 +27,20 @@ function GetNameCommander(username, unit)
     return newName
 end
 
+function GetTier(unit)
+    local found_tier
+    for _, tier in techCategories do
+        if EntityCategoryContains(categories[tier], unit) then
+            found_tier = tier
+            break
+        end
+    end
+    return found_tier
+end
+
 function RenameUnit(username, unit)
     if unit['IsChecked'] == nil and ( unit:GetCustomName(unit) == nil or unit:IsInCategory('COMMAND') == true ) then
+        local tier = GetTier(unit)
         local unitname = unit:GetBlueprint().General.UnitName
         local newName
         local temptable ;
@@ -58,9 +78,12 @@ function RenameUnit(username, unit)
         end
         if newName ~= nil then
             unit:SetCustomName(newName)
+            if tier ~= nil then
+                RenameCounts[tier] = 1 + RenameCounts[tier]
+            end
         else
             unit:SetCustomName("test")
         end
+        unit['IsChecked'] = true
     end
-    unit['IsChecked'] = true
 end
